b9f68197c31049e5d4e3391b61c8f04cd206164b
Nevan McIlwee <nmcilwee@nevada.unr.edu>
2022-04-01 20:28:46 -0700

    Hotfix- Money math operators

--- money.cpp
+++ money.cpp
@@ -15,14 +15,14 @@ Money::Money(int dollars, int cents) : all_cents{(100*dollars) + cents} {}
 //Operational Overloads
 Money Money::operator+ (const Money& rhs){
     int sum = this->all_cents + rhs.all_cents;
-    int dollars = sum%100;
-    int cents = sum - (dollars * 100);
+    int dollars = sum / 100;
+    int cents = sum % 100;
     return Money(dollars, cents);
 }
 Money Money::operator- (const Money& rhs){
     int diff = this->all_cents - rhs.all_cents;
-    int dollars = diff%100;
-    int cents = diff - (dollars * 100);
+    int dollars = diff / 100;
+    int cents = diff % 100;
     return Money(dollars, cents);
 }
 
